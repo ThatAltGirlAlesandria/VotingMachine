15d04b04cc4d8b6842c8e5369f06ef9c390c2483
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-01 15:23:26 -0700

    WIP both buttons working to display one answer.

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,21 @@ window.onload = function(){
       underAgeMessage.removeAttribute("class")
     }
   }
+    let likesCats = document.getElementById("cat-button");
+    let likesDogs = document.getElementById("dog-button");
+    let likesSticks = document.getElementById("likes-sticks");
+    let dislikesSticks = document.getElementById("dislikes-sticks");
+    if (age >=18 || likesCats &&  likes-sticks) {
+      
+  
+    }
 }
 
+//if => 18 && like cats result = indy and row
+
+
+
+//if => 18 && like dogs result = chewie and joint
+
+//if like cats && like sticks = indy and row
+//if like dogs && like sticks = chewie and joint
