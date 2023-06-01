180cc38eec552d5f2c2918640abc4d9c19701c95
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-01 13:36:25 -0700

    added js function for age

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,18 @@ window.onload = function(){
   form.onsubmit = function(e) {
     e.preventDefault();
 
+    let underAgeMessage = document.getElementById("underAge");
+    underAgeMessage.setAttribute("class", "hidden");
+    let ofAgeMessage = document.getElementById("ofAge");
+    ofAgeMessage = setAttribute("class", "hidden");
 
-    
+    let age = parseInt(document.querySelector("input#age").value);
+
+    if(age >=18){
+      ofAgeMessage.removeAttribute("class");
+    }
+    else{
+      underAgeMessage.removeAttribute("class")
+    }
   }
 }
