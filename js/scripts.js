d231baae33b2ab66700df438946c961e1e2cd05a
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-01 16:29:27 -0700

    changed logic so it links to HTML divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-window.onload = function(){
+window.onload = function () {
   const form = document.querySelector("form");
-  form.onsubmit = function(e) {
+  form.onsubmit = function (e) {
     e.preventDefault();
 
     let underAgeMessage = document.getElementById("underAge");
@@ -10,22 +10,47 @@ window.onload = function(){
 
     let age = parseInt(document.querySelector("input#age").value);
 
-    if(age >=18){
+    if (age >= 18) {
       ofAgeMessage.removeAttribute("class");
     }
-    else{
+    else {
       underAgeMessage.removeAttribute("class")
     }
   }
-    let likesCats = document.getElementById("cat-button");
-    let likesDogs = document.getElementById("dog-button");
-    let likesSticks = document.getElementById("likes-sticks");
-    let dislikesSticks = document.getElementById("dislikes-sticks");
-    if (age >=18 || likesCats &&  likes-sticks) {
-      
+  let likesCats = document.getElementById("cat-button");
+  likesCats.setAttribute("class", "hidden");
+  let likesDogs = document.getElementById("dog-button");
+  likesDogs.setAttribute("class", "hidden");
+  let likesSticks = document.getElementById("likes-sticks");
+  likesSticks.setAttribute("class", "hidden")
+  let dislikesSticks = document.getElementById("dislikes-sticks");
+  dislikesSticks.setAttribute("class", "hidden")
+  let chewieSnape = document.getElementById("chewie");
+  chewieSnape.setAttribute("class","hidden")
+  let jointP = document.getElementById("joint");
+  jointP.setAttribute("class","hidden")
+  let arrowR = document.getElementById("row");
+  arrowR.setAttribute("class","hidden")
+  let indyD = document.getElementById("Indy");
+  indyD.setAttribute("class","hidden")
   
-    }
-}
+  if (age >= 18 && likesCats && likesSticks) {
+    indyD.removeAttribute("hidden");
+
+  } else if 
+  (age >= 18 && likesCats && dislikesSticks) {
+      arrowR.removeAttribute("hidden"); 
+    
+  } else if 
+    (age >= 18 && likesDogs && likesSticks) {
+      chewieSnape.removeAttribute("hidden");
+
+  } else 
+    (age >= 18 && likesDogs && dislikesSticks)
+      jointP.removeAttribute("hidden");
+    
+};
+
 
 //if => 18 && like cats result = indy and row
 
